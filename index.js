85dea3af10bed4e0e5af398f5450e714fa555804
Lauren Auchter <lsauchter@gmail.com>
2019-05-01 23:41:03 -0400

    Added address API return and display

--- index.js
+++ index.js
@@ -2,12 +2,65 @@ const apiKey = "IkYHJ7ToHtymAllc2eCXFLhaHwxzq9CsE5ZmZTba";
 
 const baseURL = "https://developer.nps.gov/api/v1/parks";
 
+function resultAddresses (data) {
+    const addresses = []
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].addresses.length === 0) {
+            addresses.push(`<p>None</p>`);
+        }
+        if (data[i].addresses[0].type === "Physical") {
+            const fullAddress = [];
+                fullAddress.push(`<p>${data[i].addresses[0].line1}</p>`);
+            if (data[i].addresses[0].line2 != "") {
+                fullAddress.push(`<p>${data[i].addresses[0].line2}</p>`)
+            }
+            if (data[i].addresses[0].line3 != "") {
+                fullAddress.push(`<p>${data[i].addresses[0].line3}</p>`)
+            }
+                fullAddress.push(`<p>${data[i].addresses[0].city}, ${data[i].addresses[0].stateCode} ${data[i].addresses[0].postalCode}</p>`);
+            addresses.push(`${fullAddress.join("")}`);
+        }
+        if (data[i].addresses[1].type === "Physical") {
+            const fullAddress = [];
+                fullAddress.push(`<p>${data[i].addresses[1].line1}</p>`);
+            if (data[i].addresses[1].line2 != "") {
+                fullAddress.push(`<p>${data[i].addresses[1].line2}</p>`)
+            }
+            if (data[i].addresses[1].line3 != "") {
+                fullAddress.push(`<p>${data[i].addresses[1].line3}</p>`)
+            }
+                fullAddress.push(`<p>${data[i].addresses[1].city}, ${data[i].addresses[1].stateCode} ${data[i].addresses[1].postalCode}</p>`);
+            addresses.push(`${fullAddress.join("")}`);
+        }
+    }
+    return addresses
+}
+
 function displayParks(results) {
     console.log(results);
+    const addresses = resultAddresses(results);
+    console.log(addresses);
+    $(".results").empty();
+    $(".results").append(
+        `<h2>Results</h2>
+        <ul class="resultParks"></ul>`
+    );
+    $(".query").val("");
+    for (let i = 0; i < results.length; i++) {
+        $(".resultParks").append(
+            `<li><h3>${results[i].fullName}</h3>
+            <p>${results[i].description}</p>
+            <a href="${results[i].url}">${results[i].url}</a>
+            <h4>Address</h4>
+            ${addresses[i]}
+            </li>`
+        )
+    }
+    $(".results").removeClass("hidden");
 }
 
 function searchParks(query, amount) {
-    const url = baseURL + "?stateCode=" + query.join("%2C") + "&limit=" + amount + "&api_key=" + apiKey;
+    const url = baseURL + "?stateCode=" + query.join("%2C") + "&limit=" + (amount - 1) + "&fields=addresses&api_key=" + apiKey;
     
     console.log(url);
 
@@ -18,7 +71,7 @@ function searchParks(query, amount) {
         }
         throw new Error (response.statusText);
     })
-    .then(responseJson => displayParks(responseJson))
+    .then(responseJson => displayParks(responseJson.data))
     .catch(error => {
         $(".error").text(`Something went wrong: ${error.message}`);
         $(".error").removeClass("hidden");
