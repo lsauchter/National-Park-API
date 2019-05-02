12c9e147afa3ec458f049f921822e451cc85cc64
Lauren Auchter <lsauchter@gmail.com>
2019-05-01 22:23:20 -0400

    Updated styling and JS functions

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 const apiKey = "IkYHJ7ToHtymAllc2eCXFLhaHwxzq9CsE5ZmZTba";
 
-const baseURL = "developer.nps.gov/api/v1/parks";
+const baseURL = "https://developer.nps.gov/api/v1/parks";
 
-function displayParks() {
-    //DOM manipulation
-}
-
-function formatQuery (queryString) {
-    const finalString = queryString.remove(" ")
+function displayParks(results) {
+    console.log(results);
 }
 
 function searchParks(query, amount) {
-    const searchQuery = formatQuery(query)
-    const url = baseURL + "?" + 
-    //create query
-    //set search URL
-    //fetch
-    //call display function
-    //catch
+    const url = baseURL + "?stateCode=" + query.join("%2C") + "&limit=" + amount + "&api_key=" + apiKey;
+    
+    console.log(url);
+
+    fetch(url)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error (response.statusText);
+    })
+    .then(responseJson => displayParks(responseJson))
+    .catch(error => {
+        $(".error").text(`Something went wrong: ${error.message}`);
+        $(".error").removeClass("hidden");
+    })
 }
 
 
 function watchSearch() {
     $('form').submit(event => {
         event.preventDefault();
+        $(".error").addClass("hidden");
         const query = $(".query").val();
         const amount = $(".amount").val();
         searchParks(query, amount);
